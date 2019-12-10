69fdaafcf3e7c846c2e664b5f858b8c2b5fe274e
Fabricio Catae <fcatae@hotmail.com>
2019-12-10 20:50:21 -0300

    update password

--- MeuTrabalho/Controllers/AccountController.cs
+++ MeuTrabalho/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace MeuTrabalho.Controllers
         {
             try
             {
-                SqlConnection connection = new SqlConnection("Server=saturnoserver.database.windows.net;Database=MEUDB;User=aclogin;Password=homework-jan31");
+                SqlConnection connection = new SqlConnection("Server=martedb.database.windows.net;Database=sql7;User=aclogin;Password=homework-dez12");
                 SqlCommand cmd = new SqlCommand($"SELECT username FROM tbLogin WHERE email='{model.Email}' AND pwd='{model.Password}'", connection);
 
                 connection.Open();
